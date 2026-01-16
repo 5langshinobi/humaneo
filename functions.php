4185606d608eefa5b2d2f578449f18975b4d9f56
nguyen minh tu <tunm@teanis.vn>
2026-01-16 17:22:07 +0700

    sua loi

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 function miniwp_enqueue_scripts() {
-    wp_enqueue_style( 'miniwp-main-style', get_stylesheet_uri() );
+   wp_enqueue_style( 'main-style', get_stylesheet_uri(), array(), time() );
 }
 add_action( 'wp_enqueue_scripts', 'miniwp_enqueue_scripts' );
 add_theme_support( 'title-tag' );
