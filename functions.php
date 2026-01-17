61ae63189fdfb0125790c3c01b7a8187dd4b4de1
nguyen minh tu <tunm@teanis.vn>
2026-01-17 12:01:15 +0700

    first commit

--- functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-function miniwp_enqueue_scripts() {
-   wp_enqueue_style( 'main-style', get_stylesheet_uri(), array(), time() );
-}
-add_action( 'wp_enqueue_scripts', 'miniwp_enqueue_scripts' );
-add_theme_support( 'title-tag' );
-?>
